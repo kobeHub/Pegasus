97627202361ceb760a5d1b82af686d4dd71d7105
Inno Jia <jdgets111@gmail.com>
2020-04-01 18:53:47 +0800

    eidt: edit namespace migrations

--- migrations/2020-04-01-030244_namespaces/up.sql
+++ migrations/2020-04-01-030244_namespaces/up.sql
@@ -2,7 +2,7 @@ CREATE TABLE namespaces (
   id SERIAL PRIMARY KEY,
   uid UUID NOT NULL,
   namespace VARCHAR(30) NOT NULL,
-  valid BOOLEAN DEFAULT 't'
+  valid BOOLEAN NOT NULL DEFAULT 't'
 );
 
 CREATE INDEX idx_user_uuid ON namespaces (uid);
