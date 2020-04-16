3ee70f14436c3d9db4aefe2274400849cf4f69a3
Inno Jia <jdgets111@gmail.com>
2020-04-16 15:05:20 +0800

    migration: trans to repositories

--- migrations/2020-04-16-070212_repositories/up.sql
+++ migrations/2020-04-16-070212_repositories/up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE repositories (
   id SERIAL PRIMARY KEY,
   belong_to UUID,
-  repo_name VARCHAR(30) NOT NULL,
+  repo_name VARCHAR(30) NOT NULL UNIQUE,
   is_public BOOLEAN NOT NULL DEFAULT 'f'
 );
 
