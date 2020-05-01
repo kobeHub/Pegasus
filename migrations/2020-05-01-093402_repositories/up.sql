59b1cc74748c6dd2020148a3f139f2ba308be00e
Inno Jia <jdgets111@gmail.com>
2020-05-01 23:00:15 +0800

    migration: edit repository migration

--- migrations/2020-05-01-093402_repositories/up.sql
+++ migrations/2020-05-01-093402_repositories/up.sql
@@ -2,7 +2,8 @@ CREATE TABLE repositories (
   id SERIAL PRIMARY KEY,
   belong_to UUID,
   repo_name VARCHAR(30) NOT NULL UNIQUE,
-  is_public BOOLEAN NOT NULL DEFAULT 'f'
+  is_public BOOLEAN NOT NULL DEFAULT 'f',
+  is_valid BOOLEAN NOT NULL DeFAULT 't'
 );
 
 CREATE INDEX idx_belong_to ON repositories (belong_to);
